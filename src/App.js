41a450edac40ca135d99cb0f5da355d139d1711c
0akbar <S.Akbar1432@gmail.com>
2023-04-27 17:17:43 +0300

    Removed the alt attribute

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div>
       <h1>Happy 20th Birthday Zain!</h1>
-      <img src={zainImage} alt="Zain's photo" />
+      <img src={zainImage} />
       <p>
         Welcome to your birthday website! We hope you enjoy exploring and
         celebrating your special day.
