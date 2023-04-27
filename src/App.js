c196ebe809c6329f7554344c978c08e752b62843
0akbar <S.Akbar1432@gmail.com>
2023-04-27 16:53:27 +0300

    Updated the code from the boilerplate code to simple intro page to zain's birthday

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import zainImage from "./zain.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Happy 20th Birthday Zain!</h1>
+      <img src={zainImage} alt="Zain's photo" />
+      <p>
+        Welcome to your birthday website! We hope you enjoy exploring and
+        celebrating your special day.
+      </p>
     </div>
   );
 }
